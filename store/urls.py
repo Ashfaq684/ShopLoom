3c75f4ad0b822b7368de40a36c3c135e8c00cb19
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-02-17 23:33:42 +0600

    e-commerce project

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
     path('product_detail/', views.product_detail, name='product_detail'),
 ]
