7e197019149fd21c6167c1f2ee9859197f0199f0
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-02-18 23:14:49 +0600

    e-commerce project

--- shoploom/urls.py
+++ shoploom/urls.py
@@ -20,13 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('account/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
-    # path('category/', include('category.urls')),
     path('order/', include('orders.urls')),
     path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
