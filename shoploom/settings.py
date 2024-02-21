c7918c84a209a81440a4d9aa1dff54ee2cf6b30b
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-02-21 22:25:30 +0600

    e-commerce project

--- shoploom/settings.py
+++ shoploom/settings.py
@@ -133,6 +133,12 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
