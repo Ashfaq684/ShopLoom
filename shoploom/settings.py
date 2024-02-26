0cc75570aa816645b5b5ad93beb2d14177d38a04
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-02-26 21:25:55 +0600

    e-commerce project

--- shoploom/settings.py
+++ shoploom/settings.py
@@ -146,8 +146,8 @@ SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mostahid1999@gmail.com'
-EMAIL_HOST_PASSWORD = 'nrhs peah esty esgn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Default primary key field type
