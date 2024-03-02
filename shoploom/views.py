a9200ec8043ef5e27607f9d1aa092314e07646cc
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-03-02 21:07:23 +0600

    e-commerce project

--- shoploom/views.py
+++ shoploom/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from store.models import Product,ReviewRating
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('-reviewrating')
+    products = Product.objects.all().filter(is_available=True).order_by('-reviewrating')[:8]
     
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
