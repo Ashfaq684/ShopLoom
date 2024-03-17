6adf44629c951a16b47c4f4cc406a885c4e8b058
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-03-17 19:30:34 +0600

    e-commerce project

--- store/admin.py
+++ store/admin.py
@@ -9,6 +9,7 @@ class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
     extra = 1
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['product_name', 'price', 'category', 'stock', 'created_date', 'modified_date', 'is_available']
     
@@ -20,8 +21,15 @@ class VariationAdmin(admin.ModelAdmin):
     list_display = ['product', 'variation_category', 'variation_value', 'is_active']
     list_editable = ('is_active',)
     list_filter = ('product', 'variation_category')
-    
+
+
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'rating', 'created_at', 'status')
+    list_filter = ('status', 'created_at')
+    search_fields = ('product__name', 'user__username', 'subject', 'review')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
-admin.site.register(ReviewRating)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
 admin.site.register(ProductGallery)
